0508c6f4b1b408cf6dc18500811753f9e6ca2e0a
AlexOscar <alejandro.garcia.carmona@gmail.com>
2023-10-15 18:33:17 +0200

    Login Screen in Android Jetpack Compose. Show and hide passwo rd. Field validation

--- app/src/main/java/com/example/retotecnicomm/views/LoginScreen.kt
+++ app/src/main/java/com/example/retotecnicomm/views/LoginScreen.kt
@@ -1,9 +1,14 @@
 package com.example.retotecnicomm.views
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -13,6 +18,7 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,7 +28,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -45,13 +53,36 @@ fun LoginScreen(navController: NavController) {
             if (showLoginForm.value) {
                 Text(text="Iniciar Sesión")
                 UserForm(isCreateAccount = false){
-                    email, password ->
+                        email, password ->
+                    Log.d("RetoTécnicoMM", "Logueado con $email y $password")
                 }
             }else{
                 Text(text="Crear Sesión")
                 UserForm(isCreateAccount = true){
                         email, password ->
+                    Log.d("RetoTécnicoMM", "Creando con $email y $password")
                 }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                val text1 =
+                    if(showLoginForm.value) "¿No tienes cuenta?"
+                    else "¿Ya tienes cuenta?"
+                val text2 =
+                    if(showLoginForm.value) "Registrate"
+                    else "Iniciar Sesión"
+                (Text(text = text1))
+                (Text(
+                    text = text2,
+                    modifier = Modifier
+                        .clickable {showLoginForm.value = !showLoginForm.value}
+                        .padding(start = 5.dp),
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                )
             }
         }
     }
@@ -113,6 +144,7 @@ fun LoginScreen(navController: NavController) {
 //    }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UserForm(
     isCreateAccount: Boolean = false,
@@ -130,6 +162,7 @@ fun UserForm(
     val valido = remember(email.value, password.value) {
         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
     }
+    val keyboardControler = LocalSoftwareKeyboardController.current
 
     Column(horizontalAlignment = Alignment.CenterHorizontally){
         EmailInput(
@@ -143,23 +176,27 @@ fun UserForm(
         SubmitButton(
             textId = if(isCreateAccount) "Create Account" else "Login",
             inputValido = valido
-        )
+        ){
+            onDone(email.value.trim(), password.value.trim())
+            keyboardControler?.hide()
+        }
     }
 }
 
 @Composable
 fun SubmitButton(
     textId: String,
-    inputValido: Boolean
+    inputValido: Boolean,
+    onClick: () -> Unit
 ) {
     Button(
-        onClick = {/*TODO*/},
+        onClick = onClick,
         modifier = Modifier
             .padding(3.dp)
             .fillMaxWidth(),
         shape = CircleShape,
         enabled = inputValido
-        ) {
+    ) {
         Text(
             text = textId,
             modifier = Modifier
@@ -174,11 +211,11 @@ fun PasswordInput(
     labelId: String,
     passwordVisible: MutableState<Boolean>
 ) {
-val VisualTransformation = if(passwordVisible.value)
+    val VisualTransformation = if(passwordVisible.value)
         VisualTransformation.None
     else PasswordVisualTransformation()
 
-OutlinedTextField(
+    OutlinedTextField(
         value = passwordState.value,
         onValueChange = {passwordState.value = it},
         label = {Text(text = labelId)},
