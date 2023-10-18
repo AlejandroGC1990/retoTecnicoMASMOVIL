fbfe76e6aaf2e906bb45cd79f1094fa8e8a79206
AlexOscar <alejandro.garcia.carmona@gmail.com>
2023-10-18 11:48:01 +0200

    constants

--- app/src/main/java/com/example/retotecnicomm/core/Constants.kt
+++ app/src/main/java/com/example/retotecnicomm/core/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.retotecnicomm.core
 
 object Constants {
-    companion object { //Al guardar la const aquí, se compartirá en cualquier parte dónde se cite
+    object { //Al guardar la const aquí, se compartirá en cualquier parte dónde se cite
         //Screens
         const val SCREEN_SPLASH = "Splash"
         const val SCREEN_LOGIN = "Login"
@@ -15,6 +15,10 @@ object Constants {
         const val ADD = "Add"
         const val DISMISS = "Dismiss"
         const val UPDATE = "Update"
+
+        //Placeholders
+        const val IMAGE_NAME = "Image name"
+        const val IMAGE_DESCRIPTION = "Image description"
 
 
 
