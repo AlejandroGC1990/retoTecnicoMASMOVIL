31d8d0b54b4073397bc04223ef9c0f3803619563
AlexOscar <alejandro.garcia.carmona@gmail.com>
2023-10-18 11:41:35 +0200

    make folder structure

--- app/src/main/java/com/example/retotecnicomm/core/Constants.kt
+++ app/src/main/java/com/example/retotecnicomm/core/Constants.kt
@@ -1,5 +1,22 @@
 package com.example.retotecnicomm.core
 
 object Constants {
+    companion object { //Al guardar la const aquí, se compartirá en cualquier parte dónde se cite
+        //Screens
+        const val SCREEN_SPLASH = "Splash"
+        const val SCREEN_LOGIN = "Login"
+        const val SCREEN_HOME = "Home"
 
+        //Actions
+        const val ADD_IMAGE = "Add image"
+        const val REMOVE_IMAGE = "Remove image"
+
+        //Buttons
+        const val ADD = "Add"
+        const val DISMISS = "Dismiss"
+        const val UPDATE = "Update"
+
+
+
+    }
 }
